90b0b8619b8dd88b40a2bb47c12f338899c66187
jeffreyo3 <jeffrey.orndorff@gmail.com>
2020-12-15 08:03:25 -0800

    Complete Day15

--- challenges/day15.js
+++ challenges/day15.js
@@ -65,12 +65,29 @@ function memoryGame(startNums, target) {
     return prev;
 }
 
+/* 
+--- Part Two ---
+Impressed, the Elves issue you a challenge: determine the 30000000th number spoken. For example, given the same starting numbers as above:
+
+Given 0,3,6, the 30000000th number spoken is 175594.
+Given 1,3,2, the 30000000th number spoken is 2578.
+Given 2,1,3, the 30000000th number spoken is 3544142.
+Given 1,2,3, the 30000000th number spoken is 261214.
+Given 2,3,1, the 30000000th number spoken is 6895259.
+Given 3,2,1, the 30000000th number spoken is 18.
+Given 3,1,2, the 30000000th number spoken is 362.
+Given your starting numbers, what will be the 30000000th number spoken?
+*/
+
 fs.readFile(filename, "utf8", function (err, text) {
     if (err) throw err;
     console.log("OK: " + filename);
     const data = text.toString().split(",");
 
     // console.log(data);
-    const lastWordSpoken = memoryGame(data, 2020);
-    console.log("Part 1:", lastWordSpoken);
+    const lastNumSpoken = memoryGame(data, 2020);
+    console.log("Part 1:", lastNumSpoken);
+
+    const longLastNumSpoken = memoryGame(data, 30000000);
+    console.log("Part 2:", longLastNumSpoken);
 });
